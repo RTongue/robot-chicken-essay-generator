6646eca0ca39186cd43357b9fb61c95894bcc2dc
RTongue <r.tongue@gmail.com>
2017-05-24 17:07:26 -0400

    Built terminal client for making randomly generated essays based on Robot Chicken scripts

---
+++ app.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const _ = require('lodash');
+const markov = require('markov');
+const m = markov();
+
+const scripts = require('./scripts.json');
+const allText = _.reduce(scripts, (accum, episodes) => {
+  return _.reduce(episodes, (text, episode) => {
+    return text + '' + episode.toLowerCase().trim().replace(/[^a-z ]/g, '');
+  }, '');
+}, '');
+
+m.seed(allText, () => {
+    const stdin = process.openStdin();
+    console.log('> ');
+
+    stdin.on('data', (line) => {
+        const res = m.respond(line.toString(), 40)
+        for (let i = 0; i < res.length; i++) {
+          const word = res[i]
+          if (i % 10 === 0) {
+            res[i] = word[0].toUpperCase() + word.slice(1);
+          } else if (i % 10 === 9) {
+            res[i] = `${word}.`;
+          }
+        }
+        console.log(res.join(' '));
+        console.log('> ');
+    });
+});
